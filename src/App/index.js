29e82cb637c48f5fc29df8814a8defea45999b06
nursh <nsheikhmohammed@yahoo.com>
2019-01-17 10:24:45 -0500

    Added css for body and imported styled-components

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components';
+import WelcomeMessage from './WelcomeMessage';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World
-      </div>
+      <WelcomeMessage />
     );
   }
 }
